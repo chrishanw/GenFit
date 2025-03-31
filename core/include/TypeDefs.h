3820a35ad47c6652b2e8a7d8e8a262efbfee02df
Christian Wessel <christian.wessel@desy.de>
2025-03-31 11:03:20 +0200

    Few changes to TypeDefs, which I accidentally already included in the previous commit.

--- core/include/TypeDefs.h
+++ core/include/TypeDefs.h
@@ -17,8 +17,8 @@ typedef ROOT::Math::SMatrix<double, 2, 7> SMatrix27;
 typedef ROOT::Math::SMatrix<double, 3, 2> SMatrix32;
 typedef ROOT::Math::SMatrix<double, 3, 6> SMatrix36;
 
-typedef ROOT::Math::SVector<double, 2> SVector2;
-typedef ROOT::Math::SVector<double, 5> SVector5;
-
 typedef ROOT::Math::SMatrix<double, 2, 2, ROOT::Math::MatRepSym<double, 2> >    SMatrixSym2;
 typedef ROOT::Math::SMatrix<double, 5, 5, ROOT::Math::MatRepSym<double, 5> >    SMatrixSym5;
+
+typedef ROOT::Math::SVector<double, 2> SVector2;
+typedef ROOT::Math::SVector<double, 5> SVector5;
