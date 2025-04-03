f7b6bfc7c9116651be6faff489cc0bb6bb491416
Christian Wessel <christian.wessel@desy.de>
2025-04-03 10:42:00 +0200

    Add more SMatrix typedefs.

--- core/include/TypeDefs.h
+++ core/include/TypeDefs.h
@@ -12,13 +12,21 @@ typedef ROOT::Math::SMatrix<double, 2> SMatrix22;
 typedef ROOT::Math::SMatrix<double, 3> SMatrix33;
 typedef ROOT::Math::SMatrix<double, 5> SMatrix55;
 typedef ROOT::Math::SMatrix<double, 2, 3> SMatrix23;
+typedef ROOT::Math::SMatrix<double, 1, 5> SMatrix15;
 typedef ROOT::Math::SMatrix<double, 2, 5> SMatrix25;
 typedef ROOT::Math::SMatrix<double, 2, 7> SMatrix27;
+typedef ROOT::Math::SMatrix<double, 2, 12> SMatrix212;
 typedef ROOT::Math::SMatrix<double, 3, 2> SMatrix32;
+typedef ROOT::Math::SMatrix<double, 3, 3> SMatrix33;
 typedef ROOT::Math::SMatrix<double, 3, 6> SMatrix36;
+typedef ROOT::Math::SMatrix<double, 5, 1> SMatrix51;
 
 typedef ROOT::Math::SMatrix<double, 2, 2, ROOT::Math::MatRepSym<double, 2> >    SMatrixSym2;
+typedef ROOT::Math::SMatrix<double, 3, 3, ROOT::Math::MatRepSym<double, 3> >    SMatrixSym3;
 typedef ROOT::Math::SMatrix<double, 5, 5, ROOT::Math::MatRepSym<double, 5> >    SMatrixSym5;
+typedef ROOT::Math::SMatrix<double, 6, 6, ROOT::Math::MatRepSym<double, 6> >    SMatrixSym6;
 
+typedef ROOT::Math::SVector<double, 1> SVector1;
 typedef ROOT::Math::SVector<double, 2> SVector2;
 typedef ROOT::Math::SVector<double, 5> SVector5;
+typedef ROOT::Math::SVector<double, 6> SVector6;
