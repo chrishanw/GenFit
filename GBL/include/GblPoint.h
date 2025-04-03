285d359c3ae9093d255b57185ce0f628982c3db9
Christian Wessel <christian.wessel@desy.de>
2025-04-03 10:39:01 +0200

    Use SMatrix typedefs from centralised header in GblPoint.h.

--- GBL/include/GblPoint.h
+++ GBL/include/GblPoint.h
@@ -34,22 +34,14 @@
 #include<vector>
 #include<math.h>
 #include <stdexcept>
+
+#include <TypeDefs.h>
+
 #include "TVectorD.h"
 #include "TMatrixD.h"
 #include "TMatrixDSym.h"
 #include "TMatrixDSymEigen.h"
 
-#include "Math/SMatrix.h"
-#include "Math/SVector.h"
-typedef ROOT::Math::SMatrix<double, 2> SMatrix22;
-typedef ROOT::Math::SMatrix<double, 2, 3> SMatrix23;
-typedef ROOT::Math::SMatrix<double, 2, 5> SMatrix25;
-typedef ROOT::Math::SMatrix<double, 2, 7> SMatrix27;
-typedef ROOT::Math::SMatrix<double, 3, 2> SMatrix32;
-typedef ROOT::Math::SMatrix<double, 3> SMatrix33;
-typedef ROOT::Math::SMatrix<double, 5> SMatrix55;
-typedef ROOT::Math::SVector<double, 2> SVector2;
-typedef ROOT::Math::SVector<double, 5> SVector5;
 
 namespace gbl {
 
